d76273d358d5104d1506708abc4d382bd7d4af82
Dev Srivastava <devsrivastav64@gmail.com>
2026-01-01 17:55:33 +0530

    updated codes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { fetchGIF, fetchPhotos, fetchVideos } from "./api/mediaApi"
 
 const App = () => {
+
   return (
-    <div className='text-amber-400 p-4 '>
-       
+    <div className='h-screen w-full bg-gray-200'>
+       <button className="bg-amber-300 px-5 py-3 m-3" onClick={async ()=>{
+          const data = await fetchPhotos('cat')
+
+          console.log(data)
+       }}
+       >Get Photos</button>
+       <button  className="bg-amber-300 px-5 py-3 m-3" onClick={async ()=>{
+          const data = await fetchVideos('cat')
+
+         console.log(data) 
+       }}
+       >Get Videos</button>
+       <button  className="bg-amber-300 px-5 py-3 m-3" onClick={async ()=>{
+          const data = await fetchGIF('cat')
+
+         console.log(data) 
+       }}
+       >Get GIF</button>
+
     </div>
   )
 }
